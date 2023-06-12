f648774ddc92beb2b193f89ec9fc3935b1ef820c
shahbazcse <cseshahbaz@gmail.com>
2023-06-12 20:48:01 +0530

    Added Post Filter on Feed

--- codegram/src/components/NewPost.jsx
+++ codegram/src/components/NewPost.jsx
@@ -4,25 +4,28 @@ export default function NewPost() {
   const [textField, setTextField] = useState("");
   const loading = false;
   return (
-    <div className={`mt-4 px-4 ${loading && "opacity-60"}`}>
+    <div className={`mt-4 flex flex-col gap-2 px-4 ${loading && "opacity-60"}`}>
       <div className="bg-[#16181C] flex gap-2 rounded-md py-2 px-4 text-white items-center text-[20px] sticky top-1 z-10">
         <textarea
-          className="bg-transparent w-[100%] outline-none text-md h-24 "
+          className="grow resize-none bg-transparent w-[100%] outline-none text-[16px] h-24 "
           type="text"
           value={textField}
           onChange={(e) => setTextField(e.target.value)}
           placeholder="What's Happening?!"
         />
       </div>
-      <button
-        className="bg-blue-600 text-lg mt-2 py-1 px-4 rounded-md"
-        onClick={(e) => {
-          e.stopPropagation();
-          console.log("Followed User");
-        }}
-      >
-        Post
-      </button>
+      <div className="ml-auto flex items-center gap-4">
+        <button
+          type="submit"
+          className="bg-blue-600 text-lg mt-2 py-1 px-4 rounded-md"
+          onClick={(e) => {
+            e.stopPropagation();
+            console.log("Created Post");
+          }}
+        >
+          Post
+        </button>
+      </div>
     </div>
   );
 }
--- codegram/src/pages/Home.jsx
+++ codegram/src/pages/Home.jsx
@@ -8,6 +8,11 @@ export default function Home() {
         Home
       </div>
       <NewPost />
+        <div className="bg-[#16181C] p-2 m-4 flex gap-4 justify-evenly rounded-md">
+          <div onClick={() => console.log("Displaying Latest Posts")} className="hover:text-blue-600 mt-1 cursor-pointer">Latest</div>
+          <div className="text-2xl">|</div>
+          <div onClick={() => console.log("Displaying Trending Posts")} className="hover:text-red-400 mt-1 cursor-pointer">Trending</div>
+        </div>
       <Feed />
     </div>
   );
