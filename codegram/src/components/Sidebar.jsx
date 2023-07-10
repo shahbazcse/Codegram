3c8e90330671940071e13a1e95cad39272676e35
shahbazcse <cseshahbaz@gmail.com>
2023-07-10 21:08:49 +0530

    Refactored Sidebar Module

--- codegram/src/components/Sidebar.jsx
+++ codegram/src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ export default function Sidebar() {
   const userAvatar = user.avatar ? user.avatar : male;
 
   return (
-    <div className="ml-[8px] mt-0 sm:flex flex-col items-center xl:items-start xl:w-[380px] p-4 fixed h-full pr-8 xl:pr-8">
+    <div className="hidden ml-[8px] mt-0 sm:flex flex-col items-center xl:items-start xl:w-[380px] p-4 fixed h-full pr-8 xl:pr-8">
       {createModal && <CreatePostModal setCreateModal={setCreateModal} />}
       {searchModal && <SearchModalMobile setSearchModal={setSearchModal} />}
       <div className="flex items-center justify-center w-14 h-14 hoverEffect p-0 xl:mb-10 xl:ml-28">
